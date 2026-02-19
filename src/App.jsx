ecb8045c139a661c5c29b5f151286d4745ec051c
felipecoelhodev <felipecoelhoflorenciotrab@gmail.com>
2026-02-19 09:09:03 -0300

    compondo com componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import "./App.css";
-
-function FormularioDeEvento() {
-  return (
-    <form className="form-evento">
-      <h2>Preencha para criar um evento:</h2>
-      <fieldset>
-        <label htmlFor="nome">Qual o nome do evento?</label>
-        <input type="text" id="nome" />
-      </fieldset>
-    </form>
-  );
-}
+import { FormularioDeEvento } from "./componentes/FormularioDeEvento";
 
 function App() {
   return (
---
+++ src/componentes/ListaSuspensa/index.jsx
@@ -0,0 +1,9 @@
+import "./lista-suspesa.estilos.css";
+
+export function ListaSuspensa() {
+  return (
+    <select className="lista-suspensa-form">
+      <option value=""></option>
+    </select>
+  );
+}
